421d11ea5b36977ff77b8d09e50580b3e0aa6fb9
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-06-06 21:02:27 +0300

    add (main.c): adding main.c

--- MISRA/LED_Sequence/main.c
+++ MISRA/LED_Sequence/main.c
@@ -5,12 +5,18 @@
  * Author : Ahmed Hesham
  */ 
 
+#include "app.h"
 
 int main(void)
 {
     /* Replace with your application code */
+	
+	APP_initModules();
+	
+	
     while (1) 
     {
+		APP_ledSequenceV_1();
     }
 }
 
