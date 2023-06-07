3b04460b2f6d34d61fed31e8e87f2f49f02276f8
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-06-07 23:02:32 +0300

    delete (Project): unwated files

--- MISRA/LED_Sequence/Debug/ECAUL/button/button.d
+++
@@ -1,8 +0,0 @@
-ECAUL/button/button.d ECAUL/button/button.o: ../ECAUL/button/button.c \
- ../ECAUL/button/button.h ../MCAL/dio/dio_interface.h ../LIB/std.h
-
-../ECAUL/button/button.h:
-
-../MCAL/dio/dio_interface.h:
-
-../LIB/std.h:
